51ad6f5ec0da258eab9243024dc2c2a200e8d507
hoang.chau <hoang.chau@lazada.com>
2018-11-20 18:50:20 +0800

    fix views

--- celery_progress/views.py
+++ celery_progress/views.py
@@ -10,5 +10,8 @@ def get_progress(request, task_id):
     update_info = progress.get_info()
     logger.info("celery-progress views: UPDATE_INFO")
     logger.info(update_info)
-    update_info['progress']['percent'] = str(update_info['progress']['percent'])
+    try:
+        update_info['progress']['percent'] = str(update_info['progress']['percent'])
+    except TypeError:
+        pass
     return HttpResponse(json.dumps(update_info), content_type='application/json')
