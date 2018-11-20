80aa110968acf9a7f7fcbc15367dfae4c3114253
hoang.chau <hoang.chau@lazada.com>
2018-11-20 18:42:52 +0800

    fixing views

--- celery_progress/views.py
+++ celery_progress/views.py
@@ -1,10 +1,14 @@
 import json
 from django.http import HttpResponse
 from celery_progress.backend import Progress
+import logging
 
 
 def get_progress(request, task_id):
+    logger = logging.getLogger(__name__)
     progress = Progress(task_id)
     update_info = progress.get_info()
+    logger.info("celery-progress views: UPDATE_INFO")
+    logger.info(update_info)
     update_info['progress']['percent'] = str(update_info['progress']['percent'])
     return HttpResponse(json.dumps(update_info), content_type='application/json')
